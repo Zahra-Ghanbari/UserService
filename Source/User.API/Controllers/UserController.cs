e16383d26edcc05fc38d768ced685745e1be1748
Zahra Ghanbari <zahra.ghanbarinezhad@gmail.com>
2020-07-09 10:49:28 +0200

    Add User Api

---
+++ Source/User.API/Controllers/UserController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace User.API.Controllers
+{
+    [Route("api/User")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        [HttpPost]
+        public IActionResult CreateUser()
+        {
+            return null;
+
+        }
+    }
+}
