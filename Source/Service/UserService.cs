4b0016902913d48ad711dfd77410d38818439b44
Zahra Ghanbari <zahra.ghanbarinezhad@gmail.com>
2020-07-09 10:40:18 +0200

    Add Service project

---
+++ Source/Service/UserService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Interfaces;
+using Model;
+
+namespace Service
+{
+    public class UserService: IUserService
+    {
+        public UserService()
+        {
+                
+        }   
+        
+        public bool UserRegistration(User user)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
