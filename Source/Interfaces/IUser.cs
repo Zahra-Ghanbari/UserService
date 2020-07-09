73c5ce4f0a8640e1e2f6c53af5d05bbe865e404c
Zahra Ghanbari <zahra.ghanbarinezhad@gmail.com>
2020-07-09 10:46:13 +0200

    Add Interface Project

---
+++ Source/Interfaces/IUser.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Model;
+
+namespace Interfaces
+{
+    public interface IUserService
+    {
+        bool UserRegistration(User user);
+    }
+}
